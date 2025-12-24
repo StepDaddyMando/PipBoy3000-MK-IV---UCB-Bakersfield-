bceb7dc0d508ef2120078e4307b23b159bca3d07
ahern442 <afhernandez442@gmail.com>
2025-12-23 18:59:54 -0800

    Updated comments for increase clarity and conciseness

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import pygame
+pygame.init()
 
 class PipBoy:
     def __init__(self, width=800, height=480, framerate=60):
-        pygame.init()
         self.width = width
         self.height = height
         self.font= pygame.font.Font("monofonto.ttf", 12) 
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.framerate = framerate
         self.clock = pygame.time.Clock()
-        self.run_loop()
-        
+        self.run_loop() #Initializes the main loop of the PipBoy
+
+    #Main loop of the PipBoy    
     def run_loop(self):
         running = True
         while running:
@@ -24,6 +25,7 @@ class PipBoy:
 
         pygame.quit()
 
+    #Updates the entire screen with the current state of the PipBoy
     def display_update(self):
         pygame.display.flip()
     
