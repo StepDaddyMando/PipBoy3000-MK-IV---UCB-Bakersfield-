6e978f39e38caf25b877cac00a8c10e96f7a9f59
ahern442 <afhernandez442@gmail.com>
2025-12-23 18:24:19 -0800

    created bootup.txt installed font created pipboy class in test.py for testing purposes

--- test.py
+++ test.py
@@ -1,15 +1,36 @@
 import pygame
 
 class PipBoy:
-    def __init__(self, width=800, height=480):
+    def __init__(self, width=800, height=480, rate=60):
         pygame.init()
-        self.screen = pygame.display.set_mode(width, height)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (10, 10)  # Position the PipBoy at the top-left corner
-
-    def display(self):
-        self.screen.blit(self.image, self.rect)
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.rate = rate
+        self.clock = pygame.time.Clock()
+        self.run_loop()
         
 
-pipboy = PipBoy()
-pipboy.display()
+    def run_loop(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.display_update()  
+            self.clock.tick(self.rate)
+    
+
+        pygame.quit()
+
+
+
+    def display_update(self):
+        pygame.display.flip()
+    
+    
+
+
+boi= PipBoy()
+
