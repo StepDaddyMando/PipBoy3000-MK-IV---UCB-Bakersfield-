cf34a4096e41aa2ddbbaddf3213264c379b46d1c
ahern442 <afhernandez442@gmail.com>
2025-12-27 04:18:33 -0800

    Added bootup sequence function to pipboy class

--- pipboy.py
+++ pipboy.py
@@ -5,15 +5,50 @@ class PipBoy:
     def __init__(self, width=800, height=480, framerate=60):
         self.width = width
         self.height = height
-        self.font= pygame.font.Font("monofonto.ttf", 12) 
+        self.font= pygame.font.Font("monofonto.ttf", 18) 
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.framerate = framerate
         self.clock = pygame.time.Clock()
         self.run_loop() #Initializes the main loop of the PipBoy
 
+    def bootup_sequence(self):
+        self.screen.fill('black')
+
+        boot_text = ["******************************* PIP-OS (R) V7 .1.0.8 ********************************",
+                    "",
+                    "",
+                    "",
+                    "COPYRIGHT 2075 ROBCO(R)",
+                    "LOADER V1.1",
+                    "EXEC VERSION 41.10",
+                    "264k RAM SYSTEM",
+                    "253k BYTES FREE",
+                    "NO HOLOTAPE FOUND"
+                    "LOAD ROM(1): DEITRIX 303"]
+        
+        for i, line in enumerate(boot_text):
+            pos = 0
+            for j in line:
+                char_text = self.font.render(j, True, (2, 255, 2))
+                char_x = 15 + pos
+                char_y = 10 + i * (pygame.font.Font.size(self.font, "A")[1] + 4)
+                self.screen.blit(char_text, (char_x, char_y))
+                pygame.display.flip()
+                pygame.time.delay(40)
+                pos += pygame.font.Font.size(self.font, j)[0]
+    
+
     #Main loop of the PipBoy    
     def run_loop(self):
         running = True
+        bootup_done = False
+    
+        if not bootup_done:
+            self.bootup_sequence()
+            bootup_done = True
+            pygame.time.delay(2000)
+            self.screen.fill('black')
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
