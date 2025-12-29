e9e42c926a7b2708ecbe4a72545af210c7773b84
ahern442 <afhernandez442@gmail.com>
2025-12-29 11:17:14 -0800

    added flicker effect to pipboy.py

--- pipboy.py
+++ pipboy.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import math
 import menus
 
 
@@ -26,6 +28,12 @@ class PipBoy:
         self.scanline_surface = pygame.Surface(
             (self.width, self.height), pygame.SRCALPHA
         )
+        self.flicker_surface = pygame.Surface(
+            (self.width, self.height), pygame.SRCALPHA
+        )
+        self.flicker_phase = 0.0
+        self.flicker_speed = 0.6
+        self.flicker_strength = 0.03
         self.create_scanlines()
         self.framerate = framerate
         self.clock = pygame.time.Clock()
@@ -72,13 +80,26 @@ class PipBoy:
 
     def create_scanlines(self):
         self.scanline_surface.fill((0, 0, 0, 0))
-        line_spacing = 3
-        alpha_value = 35
+        line_spacing = 2
+        alpha_value = 40
 
         for y in range(0, self.height, line_spacing):
             pygame.draw.line(
                 self.scanline_surface, (0, 0, 0, alpha_value), (0, y), (self.width, y)
             )
+
+    def apply_frame_flicker(self, dt):
+        # Uses sin function to create a smooth flicker effect
+        self.flicker_phase += self.flicker_speed * dt
+        flicker = math.sin(self.flicker_phase)
+        brightness = 1.0 + (flicker * self.flicker_strength)
+        brightness = max(0.97, min(1.03, brightness))
+        value = int(255 * brightness)  
+        value = max(0, min(255, value))# converts brightness to a int, ensuring its from 0-255
+        self.flicker_surface.fill((value, value, value, 255))
+        self.screen.blit(
+            self.flicker_surface, (0, 0), special_flags=pygame.BLEND_RGBA_MULT
+        )
 
     def bootup_sequence(self):
         self.screen.fill((0, 6, 0))
@@ -127,6 +148,7 @@ class PipBoy:
 
         while running:
             self.event_handler()
+            dt = self.clock.get_time() / 1000.0
             self.tabs[self.menu_index].draw_menu(
                 self.screen,
                 self.tabs,
@@ -136,9 +158,12 @@ class PipBoy:
                 self.font_36,
                 self.font_24,
             )
+
             self.screen.blit(
                 self.scanline_surface, (0, 0)
             )  # adds scanlines after all frame elements are added to the frame
+            self.apply_frame_flicker(dt)  # applys flicker before frame is rendered
+
             pygame.display.flip()  # renders the completed frame
             self.clock.tick(self.framerate)
 
